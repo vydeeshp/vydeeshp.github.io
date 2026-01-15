5280292227035734862eaa102b9920858d29d65f
gojo_Satoru <vydeeshp@gmail.com>
2026-01-15 10:49:47 +0530

    removed browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import "./styles/style.css";
-import {Routes, Route,BrowserRouter} from "react-router-dom";
+// import {Routes, Route,BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/vydeeshp.github.io/">
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </StrictMode>
 );
